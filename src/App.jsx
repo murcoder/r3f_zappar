90df50398f8267f484dbced5aec9b32d8a169768
murcoder <chris.murauer@yahoo.de>
2025-08-21 19:04:04 +0200

    turn on/off button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ZapparCamera, ImageTracker, ZapparCanvas} from '@zappar/zappar-react-three-fiber';
 import NavBar from "@/html/NavBar";
 import {Experience} from "./components/Experience";
+import {Button} from "./components/ui/button";
 
 const targetFile = new URL('./assets/example-tracking-image.zpt', import.meta.url).href;
 
 function App () {
+  const [started, setStarted] = useState(false);
+
   return (
     <>
       <NavBar />
+      {/* Top-centered button */}
+      <div className="absolute top-4 left-1/2 -translate-x-1/2 z-[1000]">
+        <Button variant={"outline"} className={"cursor-pointer font-bold"} onClick={() => setStarted(!started)}>
+          {started ? "Stop" : "Enter AR"}
+        </Button>
+      </div>
+
+      {started && (
       <ZapparCanvas className="absolute top-0 left-0 w-full h-full">
         <ZapparCamera/>
         <ImageTracker
@@ -22,6 +33,7 @@ function App () {
         <ambientLight intensity={0.3}/>
         <directionalLight position={[2.5, 8, 5]} intensity={1.5} castShadow/>
       </ZapparCanvas>
+      )}
     </>
   );
 }
